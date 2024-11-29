e9835fbb836fc971f1633c03fe0511972d1415dd
20481A0536 <20481A0536@gecgudlavallerumic.in>
2024-11-29 21:10:52 +0530

    cleared

--- Backend/src/main/java/com/example/Project1/Model/TransactionStatus.java
+++
@@ -1,5 +0,0 @@
-package com.example.Project1.Model;
-
-public enum TransactionStatus {
-    PENDING, COMPLETED, FAILED;
-}
--- Backend/src/main/java/com/example/Project1/Repository/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Project1.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.Project1.Model.Transaction;
-
-public interface TransactionRepository extends JpaRepository <Transaction, Long>{
-    
-    
-}
